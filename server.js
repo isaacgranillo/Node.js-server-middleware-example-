4931f15050a92b0a5c16f848b9858b242344bbce
Isaac Granillo <isaac.granillo@gmail.com>
2016-03-29 16:40:23 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var port = 1337;
+
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res){
+	res.send('This is the about page')
+})
+
+app.use(express.static(__dirname + '/public'));
+app.listen(port, function(){
+	console.log('Listening on port ' + port)
+})
+
